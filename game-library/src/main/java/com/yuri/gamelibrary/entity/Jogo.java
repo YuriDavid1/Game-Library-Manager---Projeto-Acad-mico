865e8a7a763ded75d3d8b4e79a4d88b138c9128a
Yuri <Yuri111713k@gmail.com>
2026-03-01 16:49:20 -0300

    Criação da Entidade Jogo

--- game-library/src/main/java/com/yuri/gamelibrary/entity/Jogo.java
+++ game-library/src/main/java/com/yuri/gamelibrary/entity/Jogo.java
@@ -1,4 +1,46 @@
 package com.yuri.gamelibrary.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Jogo {
+
+    @Id
+    @GeneratedValue private Long id;
+    private String nome;
+    private String genero;
+    private boolean disponivel;
+
+    public Jogo(String nome, String genero) {
+        this.nome = nome;
+        this.genero = genero;
+        disponivel = true;
+    }
+
+    public Jogo(){
+    }
+
+    //Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    //Setters
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
