a4b158536fcaab1c91c46ba6f5a4b62d211e4d9e
YuriDavid1 <Yuri111713k@gmail.com>
2026-03-04 19:28:51 -0300

    Add: Service de usuario, junto das funções

---
+++ game-library/src/main/java/com/yuri/gamelibrary/service/UsuarioService.java
@@ -0,0 +1,44 @@
+package com.yuri.gamelibrary.service;
+import com.yuri.gamelibrary.entity.Usuario;
+import com.yuri.gamelibrary.repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void cadastrarUsuario(Usuario usuario){
+        if(usuario == null){
+            throw new IllegalArgumentException("Erro");
+        }
+        if(usuario.getNome() == null || usuario.getNome().isBlank()){
+            throw new IllegalArgumentException("O nome de usuário deve ser inserido.");
+        }
+        Optional<Usuario> usuEncontrado = usuarioRepository.findByNomeIgnoreCase(usuario.getNome());
+        if(usuEncontrado.isPresent()){
+            throw new IllegalArgumentException("Nome de usuário não disponível");
+        }
+        usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarUsuario(String nome){
+        Optional<Usuario> usuEncontrado = usuarioRepository.findByNomeIgnoreCase(nome);
+        if(usuEncontrado.isEmpty()){
+            throw new IllegalArgumentException("Usuario não encontrado");
+        }
+        return usuEncontrado.get();
+    }
+
+    public List<Usuario> listarTodos(){
+        if(usuarioRepository.findAll().isEmpty()){
+            throw new IllegalArgumentException("Nenhum usuário encontrado");
+        }
+        return usuarioRepository.findAll();
+    }
+}
