43f6f14771fad341e59985d88d4f15b043f9619b
YuriDavid1 <Yuri111713k@gmail.com>
2026-03-11 20:58:21 -0300

    Add:  Controller de jogo e suas funções

--- game-library/src/main/java/com/yuri/gamelibrary/controller/JogoController.java
+++ game-library/src/main/java/com/yuri/gamelibrary/controller/JogoController.java
@@ -1,4 +1,41 @@
 package com.yuri.gamelibrary.controller;
 
+import com.yuri.gamelibrary.entity.Jogo;
+import com.yuri.gamelibrary.service.JogoService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
 public class JogoController {
+
+    private final JogoService jogoService;
+
+    public JogoController(JogoService jogoService) {
+        this.jogoService = jogoService;
+    }
+
+    @PostMapping
+    public String cadastrarJogo(@RequestBody Jogo jogo) {
+        jogoService.cadastrarJogo(jogo);
+        return "Jogo cadastrado com sucesso";
+    }
+
+    @GetMapping
+    public List<Jogo> listarJogos() {
+        return jogoService.listarTodos();
+    }
+
+    @GetMapping("/by-name/{nome}")
+    public Jogo buscarJogo(@PathVariable String nome) {
+        return jogoService.buscarJogo(nome);
+    }
+
+    @DeleteMapping("/by-name/{nome}")
+    public String deletarJogo(@PathVariable String nome) {
+        jogoService.deletarJogo(nome);
+        return "Jogo deletado com sucesso";
+    }
 }
