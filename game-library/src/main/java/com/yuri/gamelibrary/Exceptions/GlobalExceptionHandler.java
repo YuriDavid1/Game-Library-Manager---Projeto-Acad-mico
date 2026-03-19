abbbd44a7debb01da67021702bae30d920a0982a
Yuri <Yuri111713k@gmail.com>
2026-03-19 15:17:22 -0300

    Add: Controle de exceções para API

--- game-library/src/main/java/com/yuri/gamelibrary/Exceptions/GlobalExceptionHandler.java
+++ game-library/src/main/java/com/yuri/gamelibrary/Exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,25 @@
 package com.yuri.gamelibrary.Exceptions;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Map<String, Object> tratarIllegalArgumentException(IllegalArgumentException ex){
 
+        Map<String, Object> erro = new HashMap<>();
+
+        erro.put("status", 400);
+        erro.put("erro", ex.getMessage());
+
+        return erro;
+    }
 
 
 }
