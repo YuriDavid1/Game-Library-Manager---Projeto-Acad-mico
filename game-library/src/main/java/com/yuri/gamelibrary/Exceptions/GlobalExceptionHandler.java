e412308cfd61416a942d245bb71f18f64e8ac496
Yuri <Yuri111713k@gmail.com>
2026-03-20 20:31:35 -0300

    Fix: Mudanças nos Exceptions

--- game-library/src/main/java/com/yuri/gamelibrary/Exceptions/GlobalExceptionHandler.java
+++ game-library/src/main/java/com/yuri/gamelibrary/Exceptions/GlobalExceptionHandler.java
@@ -1,25 +1,32 @@
 package com.yuri.gamelibrary.Exceptions;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public Map<String, Object> tratarIllegalArgumentException(IllegalArgumentException ex){
+    public ResponseEntity<Map<String, Object>> tratarIllegalArgumentException(IllegalArgumentException ex) {
 
         Map<String, Object> erro = new HashMap<>();
-
         erro.put("status", 400);
         erro.put("erro", ex.getMessage());
 
-        return erro;
+        return ResponseEntity.badRequest().body(erro);
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> tratarExceptionGenerica(Exception ex) {
 
+        Map<String, Object> erro = new HashMap<>();
+        erro.put("status", 500);
+        erro.put("erro", "Erro interno no servidor");
+
+        return ResponseEntity.internalServerError().body(erro);
+    }
 }
